454e926f547495221f2707d46e82f9de93b0109e
dispiny <aws.pjm1024cl@gmail.com>
2024-03-25 10:54:45 +0900

    Add: Base Codes

---
+++ src/main/java/org/dispiny/noticeboard/controller/mainController.java
@@ -0,0 +1,11 @@
+package org.dispiny.noticeboard.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class mainController {
+
+
+}
