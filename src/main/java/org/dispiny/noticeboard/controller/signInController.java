7eb83a4408e6307a626421ae0c48fe65b10ad75f
dispiny <aws.pjm1024cl@gmail.com>
2024-03-26 17:47:48 +0900

    Add: health api

--- src/main/java/org/dispiny/noticeboard/controller/signInController.java
+++ src/main/java/org/dispiny/noticeboard/controller/signInController.java
@@ -4,18 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class mainController {
+public class signInController {
     public static void main(String[] args) {
         SpringApplication.run(mainController.class, args);
     }
 
-    @GetMapping(value = "/api/v1/healthcheck", produces = "application/json")
-    public ResponseEntity<String> healthController() {
-        return ResponseEntity.ok().body("{\"status\": \"healthy\"}");
-    }
+    @GetMapping(value = "/api/v1/login/find", produces = "application/json")
+    public ResponseEntity<String> findController(@RequestParam(name = "typeId") String typeId) {
+        if ("test".equals(typeId)) {
+            return ResponseEntity.ok().body("{\"status\": \"healthy\"}");
+        } else {
+            return ResponseEntity.ok().body("{\"status\": \"unhealthy\"}");
+        }
 
+    }
 }
